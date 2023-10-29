4e393c3fbcce0dabbdbb991aec34252bc144c2f2
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-10-29 10:05:00 +0700

    chore: initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,48 @@
+'use client'
+
+import Image from 'next/image'
+import LogoBrand from '../public/logo_horizontal_color.png'
+import Link from 'next/link'
+
+// Icons
+import { BiSolidDownArrow } from 'react-icons/bi'
+import { BsGlobe2 } from 'react-icons/bs'
+
+function Navbar() {
+  return (
+    <header className="flex flex-row justify-between py-3 w-4/5 mx-auto">
+      <div className="flex flex-row items-center gap-10">
+        <Image
+          src={LogoBrand}
+          alt="qarir logo brand"
+          quality={100}
+          className="w-32 h-auto"
+          priority
+        />
+        <button className="flex flex-row items-center gap-3">
+          <span className="font-semibold">Course And Location</span>
+          <BiSolidDownArrow size=".6em" color="#000000" />
+        </button>
+        <Link href="/enterprise" className="font-semibold">
+          Enterprise
+        </Link>
+      </div>
+      <div className="flex flex-row items-center gap-4">
+        <Link
+          href="https://calendar.app.google/6xrDJSLWZhcYxYrs9"
+          className="bg-orange-1 flex items-center rounded-full py-2 px-5 font-bold text-white"
+        >
+          Book A Call
+        </Link>
+        <span className="text-slate-300">|</span>
+        <button className="flex flex-row items-center gap-3 font-bold">
+          <BsGlobe2 size="1.5em" color="#000000" />
+          <span>English</span>
+          <BiSolidDownArrow size=".6em" color="#000000" />
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        orange: {
+          1: '#FF9500',
+          2: '#FFB72B',
+          3: '#FFA01B',
+        },
       },
     },
   },
