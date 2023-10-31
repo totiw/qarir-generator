db421bdfa546913feab376d1b97ee52f4fd2b283
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-10-31 22:49:50 +0700

    feat: menambah component Modal
    chore: adjust component Navbar

---
+++ components/Modal.jsx
@@ -0,0 +1,9 @@
+function Modal() {
+  return (
+    <div className="fixed top-0 bottom-0 right-0 left-0 bg-slate-900 opacity-50">
+      Modal
+    </div>
+  )
+}
+
+export default Modal
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,39 +11,41 @@ import { LuMenu } from 'react-icons/lu'
 
 function Navbar() {
   return (
-    <header className="flex flex-row justify-between py-3 w-4/5 mx-auto">
-      <div className="flex flex-row items-center gap-10">
-        <Image
-          src={LogoBrand}
-          alt="qarir logo brand"
-          quality={100}
-          className="w-32 h-auto"
-          priority
-        />
-        <button className="hidden md:flex flex-row items-center gap-3">
-          <span className="font-semibold">Course And Location</span>
-          <BiSolidDownArrow size=".6em" color="#000000" />
-        </button>
-        <Link href="/enterprise" className="hidden md:flex font-semibold">
-          Enterprise
-        </Link>
-      </div>
-      <div className="flex flex-row items-center gap-4">
-        <Link
-          href="https://calendar.app.google/6xrDJSLWZhcYxYrs9"
-          className="bg-orange-1 flex items-center rounded-full py-2 px-5 font-bold text-white"
-        >
-          Book A Call
-        </Link>
-        <span className="hidden lg:flex text-slate-300">|</span>
-        <button className="hidden lg:flex flex-row items-center gap-3 font-bold">
-          <BsGlobe2 size="1.5em" color="#000000" />
-          <span>English</span>
-          <BiSolidDownArrow size=".6em" color="#000000" />
-        </button>
-        <button className="md:hidden flex">
-          <LuMenu size="1.6em" color="#000000" />
-        </button>
+    <header className="bg-white">
+      <div className="container flex flex-row justify-between py-3">
+        <div className="flex flex-row items-center gap-10">
+          <Image
+            src={LogoBrand}
+            alt="qarir logo brand"
+            quality={100}
+            className="w-32 h-auto"
+            priority
+          />
+          <button className="hidden md:flex flex-row items-center gap-3">
+            <span className="font-semibold">Course And Location</span>
+            <BiSolidDownArrow size=".6em" color="#000000" />
+          </button>
+          <Link href="/enterprise" className="hidden md:flex font-semibold">
+            Enterprise
+          </Link>
+        </div>
+        <div className="flex flex-row items-center gap-4">
+          <Link
+            href="https://calendar.app.google/6xrDJSLWZhcYxYrs9"
+            className="bg-orange-1 flex items-center rounded-full py-2 px-5 font-bold text-white"
+          >
+            Book A Call
+          </Link>
+          <span className="hidden lg:flex text-slate-300">|</span>
+          <button className="hidden lg:flex flex-row items-center gap-3 font-bold">
+            <BsGlobe2 size="1.5em" color="#000000" />
+            <span>English</span>
+            <BiSolidDownArrow size=".6em" color="#000000" />
+          </button>
+          <button className="md:hidden flex">
+            <LuMenu size="1.6em" color="#000000" />
+          </button>
+        </div>
       </div>
     </header>
   )
