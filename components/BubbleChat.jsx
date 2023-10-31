9ca9fbd9c6e339eb30544300b5a4e59f6fad8191
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-10-31 22:29:36 +0700

    chore: adjust pop up bubble chat

--- components/BubbleChat.jsx
+++ components/BubbleChat.jsx
@@ -1,9 +1,33 @@
+'use client'
+
 import Link from 'next/link'
+import { useState } from 'react'
 import { HiOutlineChatBubbleLeftRight } from 'react-icons/hi2'
+import { IoCloseCircleOutline } from 'react-icons/io5'
 
 function BubbleChat() {
+  const [isPop, seIsPop] = useState(true)
   return (
-    <div className="flex flex-row fixed z-[99] bottom-10 right-5">
+    <div className="flex flex-row gap-3 fixed z-[99] bottom-10 right-5">
+      <span
+        className={`${
+          isPop ? 'flex' : 'hidden'
+        } relative bg-white items-center gap-3 rounded-xl shadow-lg px-4`}
+      >
+        <IoCloseCircleOutline
+          onClick={() => seIsPop(false)}
+          size="1.5em"
+          color="#000000"
+          className="absolute bg-white rounded-full shadow-lg -left-10 top-0"
+        />
+        <Link
+          href="http://wa.me/+6281110014420?text=Hi%2C+I%27m+interested+to+know+about+QarirGenerator+Bootcamp%2C+My+Name+is....."
+          target="_blank"
+          className="leading-10"
+        >
+          How may i assist you to find your best career future? 🙂
+        </Link>
+      </span>
       <Link
         href="http://wa.me/+6281110014420?text=Hi%2C+I%27m+interested+to+know+about+QarirGenerator+Bootcamp%2C+My+Name+is....."
         target="_blank"
