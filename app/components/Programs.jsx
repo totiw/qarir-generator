0c5d3b3549bdb57e1a8bf74a5dcadd71a44a9237
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-11-01 22:59:53 +0700

    feat: menambahkan fungsi modal dan implement pada section program

--- app/components/Programs.jsx
+++ app/components/Programs.jsx
@@ -1,11 +1,19 @@
+'use client'
+
 // Next dependencies
+import { useState } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
 
 // Assets
-import DataScience from '../../public/data_science_2.png'
-import DigitalMarketing from '../../public/digital_marketing.png'
-import WebDev from '../../public/software_enginering.png'
+import DataScience from 'public/data_science_2.png'
+import DigitalMarketing from 'public/digital_marketing.png'
+import WebDev from 'public/software_enginering.png'
+import Indonesia from 'public/indonesia.png'
+import Instanbul from 'public/istanbul.png'
+import Korea from 'public/south_korea.png'
+import Tokyo from 'public/tokyo.jpg'
+
+import Modal from '@/components/Modal'
 
 // Icons
 import { FaArrowRight } from 'react-icons/fa6'
@@ -18,6 +26,28 @@ const dataProgram = [
     title: 'Data Science Bootcamp',
     desc: 'Learn the skills of a Data Scientist and build yours on AI products fromscratch',
     thumbnail: DataScience,
+    destinations: [
+      {
+        name: 'indonesia',
+        thumbnail: Indonesia,
+        active: true,
+      },
+      {
+        name: 'istanbul',
+        thumbnail: Instanbul,
+        active: true,
+      },
+      {
+        name: 'tokyo',
+        thumbnail: Tokyo,
+        active: false,
+      },
+      {
+        name: 'seoul',
+        thumbnail: Korea,
+        active: false,
+      },
+    ],
   },
   {
     id: 2,
@@ -25,6 +55,28 @@ const dataProgram = [
     title: 'Digital Marketing Bootcamp',
     desc: 'Learn the skills of a Digital Marketing and build yours on your company',
     thumbnail: DigitalMarketing,
+    destinations: [
+      {
+        name: 'indonesia',
+        thumbnail: Indonesia,
+        active: true,
+      },
+      {
+        name: 'istanbul',
+        thumbnail: Instanbul,
+        active: true,
+      },
+      {
+        name: 'tokyo',
+        thumbnail: Tokyo,
+        active: false,
+      },
+      {
+        name: 'seoul',
+        thumbnail: Korea,
+        active: false,
+      },
+    ],
   },
   {
     id: 3,
@@ -32,12 +84,71 @@ const dataProgram = [
     title: 'Web Development Bootcamp',
     desc: 'Learn the skills of a Software Engineering and make your product impactful',
     thumbnail: WebDev,
+    destinations: [
+      {
+        name: 'indonesia',
+        thumbnail: Indonesia,
+        active: true,
+      },
+      {
+        name: 'istanbul',
+        thumbnail: Instanbul,
+        active: true,
+      },
+      {
+        name: 'tokyo',
+        thumbnail: Tokyo,
+        active: false,
+      },
+      {
+        name: 'seoul',
+        thumbnail: Korea,
+        active: false,
+      },
+    ],
   },
 ]
 
+function Label() {
+  return (
+    <div className="absolute top-0 right-0 left-0 bottom-0 bg-slate-700 bg-opacity-50 rounded-lg">
+      <p className="absolute top-2 left-2 bg-white w-fit p-1 px-3 text-xs font-bold text-orange-1 rounded-full">
+        Soon
+      </p>
+    </div>
+  )
+}
+
 function CardProgram({ program }) {
+  const [openModal, setOpenModal] = useState(false)
+
   return (
     <div className="min-h-[32em] bg-white flex flex-col justify-between gap-5 shadow-md rounded-xl overflow-hidden">
+      <Modal isOpen={openModal} setIsOpen={setOpenModal}>
+        <div className="flex flex-row flex-wrap gap-4">
+          {program.destinations.length > 0 ? (
+            program.destinations.map((destination, index) => (
+              <div key={index} className="flex flex-col gap-2">
+                <div className="relative">
+                  {!destination.active && <Label />}
+                  <Image
+                    src={destination.thumbnail}
+                    alt="destination"
+                    quality={100}
+                    priority={false}
+                    className="w-24 h-24 rounded-lg"
+                  />
+                </div>
+                <p className="font-bold text-sm capitalize px-1">
+                  {destination.name}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>No destination</p>
+          )}
+        </div>
+      </Modal>
       <div className="flex flex-col gap-4 px-8 pt-6">
         <p
           className={`${
@@ -50,12 +161,13 @@ function CardProgram({ program }) {
         </p>
         <h3 className="text-2xl lg:text-4xl font-bold">{program.title}</h3>
         <p className="text-slate-500">{program.desc}</p>
-        <Link
-          href="#"
+        <button
+          type="button"
+          onClick={() => setOpenModal(true)}
           className="flex flex-row items-center gap-2 font-bold text-orange-1"
         >
           See Detail <FaArrowRight size="1em" color="#FF9500" />
-        </Link>
+        </button>
       </div>
 
       <Image
