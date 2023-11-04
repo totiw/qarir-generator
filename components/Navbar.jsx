2a4345fdf2f6c6dea97857212219f36c196d304d
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-11-04 13:27:29 +0700

    chore: menambahkan fungsi cek mouse klik

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 // Next dependencies
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -12,6 +12,7 @@ import Indonesia from '@/app/_components/icons/Indonesia'
 import Istanbul from '@/app/_components/icons/Istanbul'
 import Tokyo from '@/app/_components/icons/Tokyo'
 import Seoul from '@/app/_components/icons/Seoul'
+import English from '@/app/_components/icons/English'
 import Rect1 from '@/app/_components/icons/Rect1'
 import Rect2 from '@/app/_components/icons/Rect2'
 
@@ -21,6 +22,8 @@ import { BsGlobe2 } from 'react-icons/bs'
 import { LuMenu } from 'react-icons/lu'
 
 function Navbar() {
+  const menuRef = useRef(null)
+  const languageRef = useRef(null)
   const pathname = usePathname()
   const [menuOpen, setMenuOpen] = useState(false)
   const [languageOpen, setlanguageOpen] = useState(false)
@@ -84,6 +87,46 @@ function Navbar() {
     },
   ]
 
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev)
+    setlanguageOpen(false)
+  }
+
+  const toggleLanguage = () => {
+    setlanguageOpen((prev) => !prev)
+    setMenuOpen(false)
+  }
+
+  // Cek klik diluar komponen
+  useEffect(() => {
+    const handleOutSideClick = (event) => {
+      if (!menuRef.current?.contains(event.target)) {
+        setMenuOpen(false)
+      }
+    }
+
+    window.addEventListener('mousedown', handleOutSideClick)
+
+    return () => {
+      window.removeEventListener('mousedown', handleOutSideClick)
+    }
+  }, [menuRef])
+
+  // Cek klik diluar komponen
+  useEffect(() => {
+    const handleOutSideClick = (event) => {
+      if (!languageRef.current?.contains(event.target)) {
+        setlanguageOpen(false)
+      }
+    }
+
+    window.addEventListener('mousedown', handleOutSideClick)
+
+    return () => {
+      window.removeEventListener('mousedown', handleOutSideClick)
+    }
+  }, [languageRef])
+
   return (
     <header className="bg-white">
       <div className="container flex flex-row justify-between py-3">
@@ -97,11 +140,11 @@ function Navbar() {
               priority
             />
           </Link>
-          <div
-            onClick={() => setMenuOpen((prev) => !prev)}
-            className="hidden md:flex relative"
-          >
-            <button className="font-semibold flex flex-row items-center gap-3">
+          <div className="hidden md:flex relative" ref={menuRef}>
+            <button
+              onClick={toggleMenu}
+              className="font-semibold flex flex-row items-center gap-3"
+            >
               Course And Location{' '}
               <BiSolidDownArrow size=".6em" color="#000000" />
             </button>
@@ -152,12 +195,37 @@ function Navbar() {
             </Link>
           )}
           <span className="hidden lg:flex text-slate-300">|</span>
-          <div className="hidden lg:flex flex-row items-center gap-3 font-bold">
-            <button className="flex flex-row items-center gap-3 font-bold">
+          <div
+            ref={languageRef}
+            className="hidden relative lg:flex flex-row items-center gap-3 font-bold"
+          >
+            <button
+              onClick={toggleLanguage}
+              className="flex flex-row items-center gap-3 font-bold"
+            >
               <BsGlobe2 size="1.5em" color="#000000" />
               English
               <BiSolidDownArrow size=".6em" color="#000000" />
             </button>
+            <span
+              className={`${
+                languageOpen ? 'flex' : 'hidden'
+              } absolute border -bottom-14 right-6 z-0 w-10 h-10 bg-white shadow-sm rotate-45`}
+            ></span>
+            <div
+              className={`${
+                languageOpen ? 'flex' : 'hidden'
+              } absolute bottom-0 right-0 translate-y-44 flex flex-col gap-5 bg-gray-50 p-4 border rounded-md shadow-sm`}
+            >
+              <span className="flex items-center gap-2 bg-white p-3 shadow-sm rounded-md">
+                <English height="24" />
+                English
+              </span>
+              <span className="flex items-center gap-2 bg-white p-3 shadow-sm rounded-md">
+                <Indonesia height="24" />
+                Indonesia
+              </span>
+            </div>
           </div>
           <button className="md:hidden flex">
             <LuMenu size="1.6em" color="#000000" />
