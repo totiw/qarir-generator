570d6e10e3207f0065f3c3db8e3dbad67665e705
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-11-24 12:40:16 +0700

    chore: Bagian path dipendekin biar lebih ke tengah, lebih ke tengah (where start), ganti Program/ Programs jadi Programme or Programmes, Capitalize di awal button, pake bullet point di section eligibility, isi kontent talent role

--- app/career-center/_components/TalenRole.jsx
+++ app/career-center/_components/TalenRole.jsx
@@ -1,14 +1,61 @@
+'use client'
+
 // Dependencies
+import { useState } from 'react'
 import Image from 'next/image'
 
 // Assets
-import Vector5 from 'public/Vector 5.png'
+import Vector5 from 'public/talent_role.png'
 
 // Icons
 import { FaChevronRight } from 'react-icons/fa'
 import { BiSolidCircle } from 'react-icons/bi'
 
 function TalenRole() {
+  const [currentSelect, setCurrentSelect] = useState(0)
+  const dataCareerSupport = [
+    {
+      name: 'Digital Marketing',
+      points: [
+        'Social Media Specialist',
+        'Event and Campaign Management',
+        'SEO Specialist',
+        'Ads Analyst',
+        'Perfomance Marketing',
+        'And many others',
+      ],
+    },
+    {
+      name: 'Data Science',
+      points: [
+        'Machine Learning Engineer',
+        'Data Analyst',
+        'Database Administrator',
+        'Data Architect',
+      ],
+    },
+    {
+      name: 'UI/UX Design',
+      points: [
+        'User Interface Designer',
+        'User Experience Designer',
+        'Usability Tester',
+        'User Researcher',
+        'UX Strategist',
+      ],
+    },
+    {
+      name: 'FullStack Web Developer',
+      points: [
+        'Frontend Development',
+        'Backend Development',
+        'Database Management',
+        'Server Management',
+        'API Development and Integration',
+        'DevOps',
+      ],
+    },
+  ]
   return (
     <section className="container flex flex-row justify-center gap-10 mt-28">
       <div className="">
@@ -29,7 +76,39 @@ function TalenRole() {
         <p className="font-bold text-4xl">
           Find the best tech role for your team!
         </p>
-        <div className="flex flex-row gap-3 mt-5">
+        <div className="flex flex-col lg:flex-row gap-3 mt-5">
+          <div className="flex flex-col">
+            {dataCareerSupport.length > 0
+              ? dataCareerSupport.map((support, index) => (
+                  <button
+                    key={index}
+                    className={`${
+                      index === currentSelect
+                        ? 'bg-orange-1 bg-opacity-30'
+                        : 'bg-white'
+                    } hover:bg-orange-1 hover:bg-opacity-30 flex flex-row items-center justify-between gap-5 p-5 rounded-lg text-left text-xl font-semibold capitalize`}
+                    onClick={() => setCurrentSelect(index)}
+                  >
+                    <span>{support.name}</span>
+                    <FaChevronRight size="1em" color="#000" />
+                  </button>
+                ))
+              : null}
+          </div>
+          <div className="w-80 bg-orange-1 bg-opacity-10 flex flex-col gap-5 p-5 rounded-lg">
+            {dataCareerSupport[currentSelect].points.length > 0
+              ? dataCareerSupport[currentSelect].points.map((point, index) => (
+                  <p key={index} className="flex flex-row items-baseline gap-2">
+                    <span className="w-5">
+                      <BiSolidCircle size="1em" color="#FF9500" />
+                    </span>
+                    {point}
+                  </p>
+                ))
+              : null}
+          </div>
+        </div>
+        {/* <div className="flex flex-row gap-3 mt-5">
           <div className="flex flex-col">
             <button className="group flex flex-row items-center justify-between gap-5 p-5 rounded-lg bg-[#101F50] text-white text-xl font-semibold">
               <span>Digital Marketing</span>
@@ -82,7 +161,7 @@ function TalenRole() {
               And many others
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   )
